b9df4023468a488b3e7e7fe16fe4a055ac26b0ca
TruDan <>
2022-05-30 19:26:04 +0000

    d5440c7

---
+++ assets/OauthCallback.0e0838a6.js
@@ -0,0 +1,1 @@
+import{cO as e,o as n,ak as u,cP as c,y as p}from"./index.fcbd49fe.js";const k={name:"OauthCallback",setup(_){const o=c(),s=e();return n(()=>{o.callback().then(({user:t,redirectUrl:a})=>{s.replace(a!=null?a:"/")})}),(t,a)=>(p(),u("span"))}};export{k as default};
